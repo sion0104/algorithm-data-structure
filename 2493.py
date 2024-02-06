6545ca20b3d6739a61c517d1a4f8df0b81ed5168
sion0104 <sio0104@naver.com>
2024-02-06 15:08:00 +0900

    탑

--- 2493.py
+++ 2493.py
@@ -1,17 +1,19 @@
 import sys
 
 input = sys.stdin.readline
-n = int(input())
-tops = list(map(int, input().split()))
-answer = [0] * n
+
+N = int(input())
+towers = list(map(int, input().split()))
+
+result = [0] * N
 stack = []
 
-for i in range(len(tops)):
+for i in range(N):
     while stack:
-        if tops[stack[-1][0]] < tops[i]:
+        if towers[stack[-1][0]] < towers[i]:
             stack.pop()
         else:
-            answer[i] = stack[-1][0] + 1
+            result[i] = stack[-1][0] + 1
             break
-    stack.append((i, tops[i]))
-print(*answer)
+    stack.append((i, towers[i]))
+print(*result)
