80974c07c719026bf83a80dedab52541521fb53f
sion0104 <sio0104@naver.com>
2024-11-29 20:40:12 +0900

    가장 긴 바이토닉 수열

---
+++ Algorithm/DP/11054.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().rstrip().split()))
+reverse_A = A[::-1]
+
+increase_dp = [1] * N
+decrease_dp = [1] * N
+
+for i in range(N):
+    for j in range(i):
+        if A[i] > A[j]:
+            increase_dp[i] = max(increase_dp[i], increase_dp[j]+1)
+        if reverse_A[i] > reverse_A[j]:
+            decrease_dp[i] = max(decrease_dp[i], decrease_dp[j]+1)
+
+decrease_dp = decrease_dp[::-1]
+
+result = list()
+for i in range(N):
+    result.append(increase_dp[i] + decrease_dp[i]-1)
+
+print(max(result))
