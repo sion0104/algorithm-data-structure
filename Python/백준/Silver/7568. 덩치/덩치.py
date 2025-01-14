fb68b5ed38f0793ca8cd4e0be060d452d8281dc7
sion <103485710+sion0104@users.noreply.github.com>
2025-01-14 22:00:51 +0900

    [Silver V] Title: 덩치, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+students = []
+
+for _ in range(N):
+    x, y = map(int, input().split())
+    students.append((x, y))
+
+for i in students:
+    rank = 1
+
+    for j in students:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+
+    print(rank, end=' ')
+    
