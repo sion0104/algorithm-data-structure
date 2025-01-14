80fb5225e05ffd24752b86deba4e2db67e85e42e
sion <103485710+sion0104@users.noreply.github.com>
2025-01-14 22:04:27 +0900

    [Gold V] Title: 평범한 배낭, Time: 6468 ms, Memory: 229084 KB -BaekjoonHub

---
+++ Python/백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+bag = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0] * (K+1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range (1, K+1):
+        if j >= bag[i-1][0]:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-bag[i-1][0]] + bag[i-1][1])
+        else:
+            dp[i][j] = dp[i-1][j]
+
+print(dp[N][K])
