93a35f9955ca4f76ec8ccb87d607125ec8bf2be1
sion <103485710+sion0104@users.noreply.github.com>
2025-01-14 22:02:13 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def dfs(start):
+    if len(S) == M:
+        print(' '.join(map(str, S)))
+        return
+
+    for i in range(start, N + 1):
+        if i not in S:
+            S.append(i)
+            dfs(i + 1)
+            S.pop()
+
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+S = []
+dfs(1)
