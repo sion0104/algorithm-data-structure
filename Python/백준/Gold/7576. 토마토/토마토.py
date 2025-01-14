3d8c66539825dd08cfd89e36ba46c0d55762c05d
sion <103485710+sion0104@users.noreply.github.com>
2025-01-14 21:59:49 +0900

    [Gold V] Title: 토마토, Time: 2216 ms, Memory: 99616 KB -BaekjoonHub

---
+++ Python/백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,45 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+cols, rows = map(int, input().strip().split())
+
+tomatoes = []
+
+for row in range(rows):
+    tomatoes.append(list(map(int, input().strip().split())))
+
+Q = deque()
+
+for row in range(rows):
+    for col in range(cols):
+        if tomatoes[row][col] == 1:
+            Q.append((row, col))
+
+while Q:
+    length = len(Q)
+    for _ in range(length):
+        row, col = Q.popleft()
+
+        for i in range(4):
+            nr = row + dr[i]
+            nc = col + dc[i]
+
+            if rows > nr >= 0 and cols > nc >= 0 == tomatoes[nr][nc]:
+                tomatoes[nr][nc] = tomatoes[row][col] + 1
+
+                Q.append((nr, nc))
+
+day = 0
+for row in tomatoes:
+    for tomato in row:
+        if tomato == 0:
+            print(-1)
+            exit(0)
+    day = max(day, max(row))
+
+print(day-1)
