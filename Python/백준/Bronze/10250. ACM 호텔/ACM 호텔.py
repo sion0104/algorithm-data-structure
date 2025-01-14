246aa9500d5040a2850d0f81e779efb7de631bc1
sion <103485710+sion0104@users.noreply.github.com>
2025-01-14 21:48:23 +0900

    [Bronze III] Title: ACM 호텔, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+for i in range(T):
+    height, weight, num = map(int, input().split())
+
+    floor = num % height
+    room = (num // height) + 1
+    if floor == 0:
+        floor = height
+        room -= 1
+
+    print(floor * 100 + room)
+    
