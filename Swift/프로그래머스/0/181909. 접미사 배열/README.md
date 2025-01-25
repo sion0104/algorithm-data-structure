84ede7c000a7952e02587f75f383b82ee104857b
sion <103485710+sion0104@users.noreply.github.com>
2025-01-26 00:59:33 +0900

    [level 0] Title: 접미사 배열, Time: 0.52 ms, Memory: 16.7 MB -BaekjoonHub

--- Swift/프로그래머스/0/181909. 접미사 배열/README.md
+++ Swift/프로그래머스/0/181909. 접미사 배열/README.md
@@ -1,10 +1,10 @@
 # [level 0] 접미사 배열 - 181909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181909?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181909) 
 
 ### 성능 요약
 
-메모리: 16.6 MB, 시간: 0.75 ms
+메모리: 16.7 MB, 시간: 0.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 26일 00:55:14
+2025년 01월 26일 00:59:30
 
 ### 문제 설명
 
--- Swift/프로그래머스/0/181909. 접미사 배열/접미사 배열.swift
+++ Swift/프로그래머스/0/181909. 접미사 배열/접미사 배열.swift
@@ -6,8 +6,7 @@ func solution(_ my_string:String) -> [String] {
     for i in 0..<my_string.count {
         
         let startIndex = my_string.index(my_string.startIndex, offsetBy: i)
-        let endIndex = my_string.index(startIndex, offsetBy: my_string.count - i - 1)
-        let subString = my_string[startIndex...endIndex]
+        let subString = my_string[startIndex...]
 
         answer.append(String(subString))
     }
