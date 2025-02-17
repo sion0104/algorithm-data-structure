8828b634b3851c9801fc2adfe37462b633f7dbe6
sion <103485710+sion0104@users.noreply.github.com>
2025-02-17 17:18:44 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,18 @@
+def solution(numbers):
+    number_dict = {
+        "zero": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    }
+    
+    for word, digit in number_dict.items():
+        numbers = numbers.replace(word, digit)
+        
+    return int(numbers)
