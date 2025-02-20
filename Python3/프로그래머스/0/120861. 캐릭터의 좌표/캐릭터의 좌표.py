5ab6ffe58ea3b80ccac0dd03600ff03e16eb24d0
sion <103485710+sion0104@users.noreply.github.com>
2025-02-20 22:06:20 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -0,0 +1,27 @@
+def solution(keyinput, board):
+    position = [0, 0]
+    
+    max_x = (board[0] - 1) // 2
+    max_y = (board[1] - 1) // 2
+    
+    moves = {
+        "up": (0, 1),
+        "down": (0, -1),
+        "right": (1, 0), 
+        "left": (-1, 0)
+    }
+    
+    for key in keyinput:
+        dx, dy = moves[key]
+        new_x = position[0] + dx
+        new_y = position[1] + dy
+        
+        if -max_x <= new_x <= max_x:
+            position[0] = new_x
+        if -max_y <= new_y <= max_y:
+            position[1] = new_y
+        
+        
+    return position
+    
+    
