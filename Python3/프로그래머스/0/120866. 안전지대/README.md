a3739ba9405ab74349964a1f6cbf6cb42dc0a9a4
sion <103485710+sion0104@users.noreply.github.com>
2025-02-26 13:43:06 +0900

    [level 0] Title: 안전지대, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/0/120866. 안전지대/README.md
+++ Python3/프로그래머스/0/120866. 안전지대/README.md
@@ -1,10 +1,10 @@
 # [level 0] 안전지대 - 120866 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120866) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120866?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.12 ms
+메모리: 10.2 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 25일 22:15:59
+2025년 02월 26일 13:43:01
 
 ### 문제 설명
 
--- Python3/프로그래머스/0/120866. 안전지대/안전지대.py
+++ Python3/프로그래머스/0/120866. 안전지대/안전지대.py
@@ -1,18 +1,20 @@
 def solution(board):
     n = len(board)
-    directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-
+    directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1) ]
+    
     danger_zones = [[0] * n for _ in range(n)]
-
+    
     for i in range(n):
         for j in range(n):
             if board[i][j] == 1:
                 danger_zones[i][j] = 1
+                
                 for direction in directions:
                     ni, nj = i + direction[0], j + direction[1]
+                    
                     if 0 <= ni < n and 0 <= nj < n:
                         danger_zones[ni][nj] = 1
-
+                        
     safe_count = sum(danger_zones[i][j] == 0 for i in range(n) for j in range(n))
-
+    
     return safe_count
