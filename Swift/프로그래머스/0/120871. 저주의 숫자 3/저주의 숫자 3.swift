0a7e0dcbb3e5c4e011141535fbdf09001c461419
sion <103485710+sion0104@users.noreply.github.com>
2025-02-28 22:31:43 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.28 ms, Memory: 16.4 MB -BaekjoonHub

--- Swift/프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.swift
+++ Swift/프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.swift
@@ -1,16 +1,15 @@
 import Foundation
 
-func solution(_ n: Int) -> Int {
-    var result = 0
+func solution(_ n:Int) -> Int {
     var number = 0
+    var result = 0
     
-    for _ in 1...n {
+    for i in 1...n {
         number += 1
         while (number % 3 == 0 || String(number).contains("3")) {
             number += 1
         }
         result = number
     }
-    
     return result
 }
