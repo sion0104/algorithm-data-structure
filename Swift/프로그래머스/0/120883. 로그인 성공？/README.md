da7aa831c6f3384a31f337d02a92f3b1c20c59da
sion <103485710+sion0104@users.noreply.github.com>
2025-03-04 22:18:31 +0900

    [level 0] Title: 로그인 성공?, Time: 0.05 ms, Memory: 16.2 MB -BaekjoonHub

--- Swift/프로그래머스/0/120883. 로그인 성공？/README.md
+++ Swift/프로그래머스/0/120883. 로그인 성공？/README.md
@@ -1,10 +1,10 @@
 # [level 0] 로그인 성공? - 120883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120883) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120883?language=swift) 
 
 ### 성능 요약
 
-메모리: 16.1 MB, 시간: 0.04 ms
+메모리: 16.2 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 04일 17:47:51
+2025년 03월 04일 22:18:28
 
 ### 문제 설명
 
--- Swift/프로그래머스/0/120883. 로그인 성공？/로그인 성공？.swift
+++ Swift/프로그래머스/0/120883. 로그인 성공？/로그인 성공？.swift
@@ -1,16 +1,17 @@
 import Foundation
 
 func solution(_ id_pw:[String], _ db:[[String]]) -> String {
-    if db.contains(id_pw) {
-        return "login"
-    } else {
-        let id = id_pw[0]
-
-        for info in db {
-            if info[0] == id {
-                return "wrong pw"
-            }
-        }
-        return "fail"
+    enum LoginResult {
+        static let success = "login"
+        static let wrongPassword = "wrong pw"
+        static let fail = "fail"
     }
+    
+    let (id, password) = (id_pw[0], id_pw[1])
+    
+    guard let userInfo = db.first(where: {$0[0] == id} ) else {
+        return LoginResult.fail
+    }
+    
+    return userInfo[1] == password ? LoginResult.success : LoginResult.wrongPassword
 }
