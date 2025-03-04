e917f37bb9632013701631face29258923ccb109
sion <103485710+sion0104@users.noreply.github.com>
2025-03-04 13:16:56 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/0/120882. 등수 매기기/등수 매기기.py
+++ Python3/프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -1,9 +1,19 @@
 def solution(score):
-    averages = [sum(s)/2 for s in score]
+    avg_list = [(sum(s)/2, i) for i, s in enumerate(score)]
+    avg_list.sort(reverse= True)
     
-    ranks = []
-    for avg in averages:
-        rank = 1 + sum(a > avg for a in averages)
-        ranks.append(rank)
+    ranks = [0] * len(score)
+    cur_rank = 1
+    prev_score = None
+    same_rank_count = 0
+    
+    for avg, idx in avg_list:
+        if prev_score != avg:
+            cur_rank += same_rank_count
+            same_rank_count = 1
+        else:
+            same_rank_count += 1
+        ranks[idx] = cur_rank
+        prev_score = avg
         
     return ranks
