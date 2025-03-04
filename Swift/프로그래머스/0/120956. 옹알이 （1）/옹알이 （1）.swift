94d4d36797eba28cfa9b41a48e52a6d0c1ee7b19
sion <103485710+sion0104@users.noreply.github.com>
2025-03-04 15:48:30 +0900

    [level 0] Title: 옹알이 (1), Time: 4.25 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ Swift/프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ babbling:[String]) -> Int {
+    var possible = ["aya", "ye", "woo", "ma"]
+    
+    return babbling.filter { word in
+        var temp = word     
+                            
+        for sound in possible {
+            temp = temp.replacingOccurrences(of: sound, with: " ")
+        }
+                            
+        return temp.trimmingCharacters(in: .whitespaces).isEmpty
+    }.count
+}
