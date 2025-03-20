0b1daf501cb744bb08d988fab1cffd2f01b36e88
sion <103485710+sion0104@users.noreply.github.com>
2025-03-21 00:44:38 +0900

    [level 2] Title: 타겟 넘버, Time: 391.92 ms, Memory: 9.12 MB -BaekjoonHub

--- Python3/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ Python3/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,10 +1,11 @@
+# 순서를 바꾸지 않고 연산해 타겟 넘버 만들기
 def solution(numbers, target):
     def dfs(index, current_sum):
         if index == len(numbers):
             return 1 if current_sum == target else 0
-    
-        plus = dfs(index+1, current_sum + numbers[index])
-        minus = dfs(index+1, current_sum - numbers[index])
+        
+        plus = dfs(index + 1, current_sum + numbers[index])
+        minus = dfs(index + 1, current_sum - numbers[index])
         
         return plus + minus
     
