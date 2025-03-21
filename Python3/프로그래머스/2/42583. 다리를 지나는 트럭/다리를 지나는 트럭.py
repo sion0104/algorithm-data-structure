4c304ccd6d114d20e85e3fd5ecde633dab31c8b7
sion <103485710+sion0104@users.noreply.github.com>
2025-03-21 11:11:06 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 62.13 ms, Memory: 9.34 MB -BaekjoonHub

--- Python3/프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ Python3/프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,3 +1,7 @@
+# 정해진 순서로(queue) 다리 건너기
+# 반환값: 모든 트럭이 다리를 건너기 위한 최소 시간
+
+# 최대 다리에 올라갈 수 있는 트럭의 개수, 다리에 올라갈 수 있는 최대 무게, 트럭 무게 배열
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
@@ -7,25 +11,23 @@ def solution(bridge_length, weight, truck_weights):
     bridge_weight = 0
     time = 0
     
-    # 첫 트럭 진입
     if trucks:
         truck = trucks.popleft()
-        bridge[-1] = truck
         bridge_weight += truck
-    
-    while bridge_weight > 0:  # 다리 위에 트럭이 있는 동안만 반복
+        bridge[-1] = truck
+        
+    while bridge_weight > 0:
         time += 1
         
-        # 다리에서 트럭 빼기
         bridge_weight -= bridge[0]
         bridge.popleft()
         
-        # 새 트럭 추가 가능한지 확인
-        if trucks and bridge_weight + trucks[0] <= weight:
+        if trucks and trucks[0] + bridge_weight <= weight:
             truck = trucks.popleft()
             bridge.append(truck)
             bridge_weight += truck
         else:
             bridge.append(0)
-    
-    return time + 1  # 마지막 트럭이 다리를 완전히 건너는 시간 추가
+            
+    return time + 1
+        
