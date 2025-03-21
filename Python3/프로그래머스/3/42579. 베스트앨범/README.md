1496962f829b03041646145fe1c76a55ce781783
sion <103485710+sion0104@users.noreply.github.com>
2025-03-21 10:16:44 +0900

    [level 3] Title: 베스트앨범, Time: 0.08 ms, Memory: 9.14 MB -BaekjoonHub

--- Python3/프로그래머스/3/42579. 베스트앨범/README.md
+++ Python3/프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.16 MB, 시간: 0.07 ms
+메모리: 9.14 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 19일 22:55:25
+2025년 03월 21일 10:16:40
 
 ### 문제 설명
 
--- Python3/프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ Python3/프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,16 +1,22 @@
+# 장르별로 2개 씩
+# 노래: 고유번호
+
+# 1. 속한 노래가 많이 재생된 장르(장르별 총 재생 횟수가 큰 순)
+# 2. 해당 장르 내에서 가장 많이 재생된 음악
+# 3. 횟수가 같은 경우 고유번호가 낮은 거 부터(index 번호가 낮은 것)
+
+# 앨범에 수록될 고유번호
+
 def solution(genres, plays):
-    songs = {}
-    total_plays = {}
+    # 장르별 재생 횟수, 장르별 고유 번호와 재생 횟수
+    total_play, genre_song = {}, {}
     
-    for i, (genre, play) in enumerate(zip(genres, plays)):
-        if genre not in songs:
-            songs[genre] = []
-        songs[genre].append((i, play))
-        total_plays[genre] = total_plays.get(genre, 0) + play
+    for index, (genre, play) in enumerate(zip(genres, plays)):
+        genre_song[genre] = genre_song.get(genre, []) + [(index, play)]
+        total_play[genre] = total_play.get(genre, 0) + play
         
     answer = []
-    for genre in sorted(total_plays, key=total_plays.get, reverse=True):
-        for i in sorted(songs[genre], key=lambda x: (-x[1], x[0]))[:2]:
-            answer.append(i[0])
+    for genre in sorted(total_play, key=total_play.get, reverse=True): 
+        answer.extend([index for index, play in sorted(genre_song[genre], key=lambda x:(-x[1], x[0]))][:2])
     
     return answer
