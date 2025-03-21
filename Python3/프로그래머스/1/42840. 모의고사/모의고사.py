4c7efe8f4558e3d71bbcdf2f559df33c4ee75807
sion <103485710+sion0104@users.noreply.github.com>
2025-03-21 14:24:50 +0900

    [level 1] Title: 모의고사, Time: 3.21 ms, Memory: 9.47 MB -BaekjoonHub

--- Python3/프로그래머스/1/42840. 모의고사/모의고사.py
+++ Python3/프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,22 +1,19 @@
 def solution(answers):
-    # 1, 2, 3, 4, 5
-    # 2, 1, 2, 3, 2, 4, 2, 5
-    # 3, 3, 1, 1, 2, 2, 4, 4, 5, 5
+    answer1 = [1, 2, 3, 4, 5]
+    answer2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    answer3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    first = [1, 2, 3, 4, 5]
-    second = [2, 1, 2, 3, 2, 4, 2, 5]
-    third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score = [0, 0, 0]
     
-    scores = [0, 0, 0]
     for i, answer in enumerate(answers):
-        if first[i % len(first)] == answers[i]:
-            scores[0] += 1
-        if second[i % len(second)] == answers[i]:
-            scores[1] += 1
-        if third[i % len(third)] == answers[i]:
-            scores[2] += 1
+        if answer1[i % len(answer1)] == answer:
+            score[0] += 1
+        if answer2[i % len(answer2)] == answer:
+            score[1] += 1
+        if answer3[i % len(answer3)] == answer:
+            score[2] += 1
+        
+    # 반환값: 가장 많이 문제를 맞춘 사람의 번호 배열
+    max_score = max(score)
+    return [i + 1 for i in range(len(score)) if score[i] == max_score]
             
-    max_score = max(scores)
-    result = [i + 1 for i in range(len(scores)) if scores[i] == max_score]
-    
-    return result
