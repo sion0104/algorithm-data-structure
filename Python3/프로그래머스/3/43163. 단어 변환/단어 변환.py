5a9c4be451a69eb7501ddc70bc3d9361bfac5034
sion <103485710+sion0104@users.noreply.github.com>
2025-03-21 21:30:01 +0900

    [level 3] Title: 단어 변환, Time: 0.36 ms, Memory: 9.25 MB -BaekjoonHub

--- Python3/프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ Python3/프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,3 +1,9 @@
+# 한 번씩만 변환
+# words에 포함된 단어
+# begin -> target
+
+# 최소 과정 구하기(BFS)
+# 변환할 수 없는 경우 0 return
 from collections import deque
 
 def solution(begin, target, words):
@@ -5,26 +11,25 @@ def solution(begin, target, words):
         return 0
     
     queue = deque([(begin, 0)])
-    visited = set() 
+    visited = set()
+    
     
     while queue:
-        current, steps = queue.popleft()
+        current, count = queue.popleft()
         
         if current == target:
-            return steps
+            return count
         
         for word in words:
-            if word not in visited and can_change(current, word):
+            if can_change(current, word) and word not in visited:
+                queue.append((word, count + 1))
                 visited.add(word)
-                queue.append((word, steps + 1))
-                
     return 0
-    
+
 def can_change(word1, word2):
     diff = 0
-    
-    for c1, c2 in zip(word1, word2):
-        if c1 != c2:
+    for char1, char2 in zip(word1, word2):
+        if char1 != char2:
             diff += 1
     
     return diff == 1
