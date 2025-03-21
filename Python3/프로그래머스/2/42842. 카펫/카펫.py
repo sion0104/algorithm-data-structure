2a20ee49fbacfc9bb1ef543d805780321cff657d
sion <103485710+sion0104@users.noreply.github.com>
2025-03-21 14:47:42 +0900

    [level 2] Title: 카펫, Time: 0.12 ms, Memory: 9.16 MB -BaekjoonHub

--- Python3/프로그래머스/2/42842. 카펫/카펫.py
+++ Python3/프로그래머스/2/42842. 카펫/카펫.py
@@ -1,14 +1,17 @@
+# 색깔 별 격자 개수
 def solution(brown, yellow):
-    if yellow == 1:
-        return [3, 3]
+        if yellow == 1:
+            return [3, 3]
         
-    for i in range(1, yellow//2 + 1):
-        if yellow % i != 0:
-            continue
+        for i in range(1, yellow // 2 + 1):
+            if yellow % i != 0:
+                continue
             
-        width, height = yellow // i, i
-        
-        brown_count = (width + 2) * 2 + height * 2
-        
-        if brown_count == brown:
-            return [width + 2, height + 2]
+            width, height = yellow // i, i
+            
+            brown_count = (width + 2) * 2 + height * 2
+            
+            if brown_count == brown: 
+                return [width + 2, height + 2]
+            
+        # 반환값 [가로, 세로]
