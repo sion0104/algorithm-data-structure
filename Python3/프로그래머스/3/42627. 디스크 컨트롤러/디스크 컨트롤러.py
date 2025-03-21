74df3497fc223b8f2767e76a4b4569e86f6535bd
sion <103485710+sion0104@users.noreply.github.com>
2025-03-21 12:22:16 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.59 ms, Memory: 9.43 MB -BaekjoonHub

--- Python3/프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ Python3/프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,8 +1,10 @@
-# 대기 큐
-# 1. 우선 순위(소요시간 짧은 것, 작업 시간의 요청 시각이 빠른 것, 작업 번호가 작은 것 순)
-# 마칠 때까지 그 작업만 수행
+# 대기큐: [작업 번호, 요청 시각, 소요 시간]
+# 처음은 empty
 
-# index: 작업 번호[작업 요청 시각, 소요 시간]
+# 우선순위 순으로 작업 수행
+# 소요 시간 짧은 것 -> 작업 요청 시각 -> 작업 번호 작은 것
+
+# jobs [요청 시점, 소요 시간] index: 작업 번호
 import heapq
 
 def solution(jobs):
@@ -17,7 +19,7 @@ def solution(jobs):
         while job_index < length and jobs[job_index][0] <= current_time:
             heapq.heappush(waiting, (jobs[job_index][1], jobs[job_index][0]))
             job_index += 1
-            
+        
         if waiting:
             duration, request_time = heapq.heappop(waiting)
             current_time += duration
@@ -25,7 +27,7 @@ def solution(jobs):
         else:
             if job_index < length:
                 current_time = jobs[job_index][0]
-                
+        
     return total_time // length
-        
-        
+# 반환값: 반환 시간의 평균의 정수 부분
+# 반환 시간 : 작업 요청부터 종료까지 걸린 시간
