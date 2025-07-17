af91b3af906541bb4598327fda8367876ac80c84
sion <103485710+sion0104@users.noreply.github.com>
2025-07-17 20:14:47 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- Python3/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
+++ Python3/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
@@ -1,10 +1,10 @@
 # [level 1] 로또의 최고 순위와 최저 순위 - 77484 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.27 MB, 시간: 0.01 ms
+메모리: 9.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 17일 20:03:24
+2025년 07월 17일 20:14:44
 
 ### 문제 설명
 
--- Python3/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ Python3/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,23 +1,16 @@
 def solution(lottos, win_nums):
     count = 0
-    answer = []
     
     for win_num in win_nums:
-        if win_num == 0:
-            continue
         if win_num in lottos :
             count += 1
-            
-    if count > 1:
-        answer.append(7-count)
-    else:
-        answer.append(6)
     
     zero_count = lottos.count(0)
-    if zero_count + count > 1:
-        answer.append(7-zero_count-count)
-    else:
-        answer.append(6)
+    
+    maxRank = min(6, 7-(zero_count + count))
+    
+    minRank = min(6, 7-count)
+
         
-    return sorted(answer)
+    return [maxRank, minRank]
     
